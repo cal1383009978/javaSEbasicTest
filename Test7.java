5bfa79e68cc0cb5c500cf6adfafb00ad263dca6d
gg <1067655162@qq.com>
2019-08-03 00:20:45 +0800

    give me

---
+++ Test7.java
@@ -0,0 +1,12 @@
+public class ArrayDemo{
+	public static void main(String[] args){
+		byte[] nums = {1,5,7,9,10};
+		//byte[] nums2 = new byte[]
+		/*for(int i=0;i<nums.length;i++)
+		{
+			System.out.print(nums[i]);
+		}*/
+		System.out.print(nums[5]);
+	}
+}
+		
