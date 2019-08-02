2403c2b55844d2ba77f3a702e8a2ce96507c8dff
gg <1067655162@qq.com>
2019-08-03 00:16:11 +0800

    first commit

---
+++ VarDemo.java
@@ -0,0 +1,92 @@
+//import java.util.Random;
+//import java.util.Random;
+//import java.util.Scanner;
+//import java.util.Scanner;
+//import static java.lang.Math.*;
+//impport static java.lang.Math.*;
+//public class VarDemo(){
+import javax.swing.*;
+public class VarDemo{
+    //public static void main(String[] args)
+	public static void main(String[] args)
+    {
+	//{
+	   // int age = 50;
+	   //if(age>20){
+	   //	int score= 100;
+	   //	System.out.print(score);
+		
+		//}
+		//System.out.print(age);
+		//int age =50;
+		//if(age>20){
+			//int score = 100;
+			//System.out.print(age);
+		//}
+		//System.out.print(age);
+		
+		//byte num = (byte)128;
+		//byte num = (byte)129;
+		//byte num = (byte)130;
+		//System.out.print(num);
+		//byte num = (byte)128;
+		//byte num = (byte)127;
+		//byte num = (byte)126;
+		//System.out.print(num);
+		
+		  int i = 19;
+		//System.out.println(Integer.toBinaryString(i));
+		//System.out.println(Integer.toHexString(i));
+		  //System.out.println(Integer.toOctalString(i));
+		  //int i = 19;
+		  //System.out.println(Integer.toBinaryString(i));
+		  //System.out.println(Integer.toHexString(i));
+		  System.out.println(Integer.toOctalString(i));
+		
+		//long a = 13241234856854L;
+		//System.out.print(a);
+		//  long a = 12345678901L;//编译错误
+		 // System.out.print(a);
+		 
+		 //float num = 3.1415F;
+		 //System.out.print(num);
+		 //float num = 3.1415926F;
+		// System.out.print(num);
+		//double d = 1234567890.0987654321;
+		//System.out.println(d);
+		
+		//int age = 50;
+		//boolean result = true;
+		//if(age >=50)
+		//{
+			//result = false;
+		//}
+		//System.out.print(result);
+		
+		//Random r = new Random();
+		//int i = r.nextInt(26);
+		//char ch = (char) (i+'A');
+		//System.out.println(ch);
+		
+		//char ch1 = 97;
+		//System.out.println(ch1);
+		
+		//char ch2 = '0';
+		//int i2 = ch2;
+		//System.out.println(ch2);
+		
+		//Scanner scanner=new Scanner(System.in);
+		//System.out.println("请输入一个数：");
+		//int num=scanner.nextInt();
+		//int squ=num*num;
+		//System.out.println(num*num);
+		
+		//Long num1 = 39735360000000000L;
+		   // double num2 = 0.45;
+			//double num3 = num1/num2;
+			//System.out.print(num3);
+		
+		 
+		
+	}
+}
