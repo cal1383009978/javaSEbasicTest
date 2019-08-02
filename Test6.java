4bb848f4485d21df442d5dcd492da85be1e7d8a2
gg <1067655162@qq.com>
2019-08-03 00:18:47 +0800

    love u

---
+++ Test6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Test6{
+	public static void main(String[] args){
+	//whileÑ­»·
+	//1-100µÄºÍ
+	/*int i = 1;
+	int sum = 0;
+	while(i<=100){
+		sum = sum +i;
+		i++;
+	}
+	System.out.print(sum);*/
+	
+	for(int i=0;i<=100;++i){
+		System.out.print(i);
+	}
+	
+	int i = 1;
+	do{
+		System.out.print("do...");
+	}while(i<1);
+    }
+}
