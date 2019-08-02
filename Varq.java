fa4453e996096618ba52bab05ab22e2fa9f10160
gg <1067655162@qq.com>
2019-08-03 00:12:33 +0800

    first commit

---
+++ Varq.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Varq{
+    public static void main(String[] args)
+    {
+        Random r = new Random();
+		int i = r.nextInt(26);
+		char ch = (char) (i+'A');
+		System.out.println(ch);
+	}
+}
